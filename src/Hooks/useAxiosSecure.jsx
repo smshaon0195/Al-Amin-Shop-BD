df824e897127fd696652e7f3a16055e295b3045e
smshaon0195 <smshaonhossen@gmail.com>
2026-05-27 17:38:35 +0600

    dist folder

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://al-amin-shop-bd-server.onrender.com",
 });
 
 const useAxiosSecure = () => {
